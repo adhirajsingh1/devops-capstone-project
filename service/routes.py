9a11f68400cbea68bd8710b443147fd895b6a794
adhirajsingh1 <166180287+adhirajsingh1@users.noreply.github.com>
2024-07-19 16:45:46 +0530

    Update routes.py

--- service/routes.py
+++ service/routes.py
@@ -12,6 +12,7 @@ from . import app  # Import Flask application
 ############################################################
 # Health Endpoint
 ############################################################
+
 @app.route("/health")
 def health():
     """Health Status"""
@@ -77,6 +78,7 @@ def get_accounts(account_id):
     if not account:
         abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
     return account.serialize(), status.HTTP_200_OK
+
 
 def test_get_account_not_found(self):
     """It should not Read an Account that is not found"""
